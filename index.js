afb1203b3d16481de65a3329e35837e22559e923
ChrisanerM <chrisanermatthys04@gmail.com>
2023-02-16 15:43:47 +0200

    fixing errors

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+ const express = require("express");
 //path
 const path = require("path");
 //db
@@ -20,7 +20,7 @@ route.get("/", (req, res) => {
 });
 
 //Retrieve all users
-route.get('/users', (req, res) => {
+route.get("/users", (req, res) => {
   const strQry = `SELECT firstName,lastName, emailAdd, country
     FROM Users;
     `;
@@ -32,23 +32,67 @@ route.get('/users', (req, res) => {
 });
 
 //Register
-route.post('/register',bodyParser.json(), (req, res)=> {
+route.post("/register", bodyParser.json(), (req, res) => {
   let detail = req.body;
   console.log(detail);
-  //sql query
-  const strQry = 
-  `INSERT INTO Users
-   SET ?;`;
-
-  db.query(strQry, [detail], (err) => {
-    if (err) {
-      res.status(400).json({ err });
-    } else {
-      res.status(200).json({ msg: "A new user record was saved" });
-    }
-  });
 });
 
+//UPDATE
+route.put("/user/:id", bodyParser.json(), (req, res)=>{
+  let det = req.body;
+  const strQry = 
+  `
+  UPDATE Users
+  SET?
+  WHERE userID= ?;
+  `;
+  //db
+  db.query(strQry,[data,req.params.id],(err, data) => {
+    if (err) throw err;
+    res.status(200).json({msg: "A new row was affected" });
+  })
+
+});
+//DELETE
+route.delete("/user/:id", bodyParser.json(), (req, res) => {
+  const strQry = 
+  `
+  DELETE FROM Users
+  SET?
+  WHERE userID= ?;
+  `;
+  db.query(strQry,[req.params.id],(err, data) => {
+    if (err) throw err;
+    res.status(200).json({msg: "A new row was affected" });
+  })
+});
+// Login
+route.patch('/login', bodyParser.json(), (req, res)=>{
+    const {emailAdd, userPass} = req.body;
+    const strQry =
+    `
+    SELECT firstName, lastName, emailAdd, userPass,
+    country
+    FROM Users
+    WHERE emailAdd = '${emailAdd}';
+    `;
+    db.query(strQry, (err, data)=>{
+        if(err) throw err;
+        if((!data.length) || (data == null)) {
+            res.status(401).json({err:
+                "You provide a wrong email address"});
+        }else {
+            let {firstName, lastName} = data[0];
+            if(userPass === data[0].userPass) {
+                res.status(200).json({msg:
+                    `Welcome back, ${firstName} ${lastName}`});
+            }else {
+                res.status(200).json({err:
+                    `You provide a wrong password`});
+            }
+        }
+    })
+})
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
 });
